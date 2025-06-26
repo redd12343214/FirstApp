1bb6a6a0e72fd059d026f7d8b6619dacc2e5dec3
Ehigbai Peter Iriabije <justatech.redd@gmail.com>
2025-06-26 20:56:01 +0100

    Environment variable properly defined.

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[f"{frontend}"],  # React ports
+    allow_origins=[f"{os.environ.get('frontend')}"],  # React ports
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
